dfa6c1fb550daeeba5162ea883d99628ff7724db
Thiago Sardinha Correia <thi-correia@uol.com.br>
2025-11-23 10:12:15 -0300

    Adicionado projeto básico

---
+++ func4_backend/src/main/java/br/com/func4_backend/func4_backend/Func4BackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.func4_backend.func4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Func4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Func4BackendApplication.class, args);
+	}
+
+}
---
+++ func4_backend/src/test/java/br/com/func4_backend/func4_backend/Func4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.func4_backend.func4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Func4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
